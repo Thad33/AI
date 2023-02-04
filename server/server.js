33a73bad1cb595c28c150549e6dae96eedbef154
Thaddaeus <thaddaeusotieno@gmail.com>
2023-02-04 23:17:56 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,46 @@
 import express from 'express';
 import * as dotenv from 'dotnev';
 import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello World!',
+    });
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const promt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+        res.status(200).send({
+            bot: response.data.choice[0].text
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            error: error
+        })
+    }
+})
+
+app.listen( 5000, () =>  console.log('Server is running on port http://localhost:5000'));
